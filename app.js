8cd221f493ea5a69ab61f6da99e08b2eb78e14d6
Juan Restrepo <juan.restrepo@globant.com>
2019-09-04 21:07:34 -0500

    Finished graphql server

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./server/schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// Allow Cross-Origin requests
+app.use(cors());
 
 const URL = 'mongodb+srv://admin:admin1234@gql-tutorial-erbks.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(URL, {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
   console.log('Conected to database')
 })
+
+var x = mongoose.Types.ObjectId()
+
+console.log('log',x)
 
 app.use('/graphql', graphqlHTTP({
   schema,
